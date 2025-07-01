930f1ba7b710b9ae67c2dc4af2efe34bd171139e
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-07-01 19:25:17 +0200

    Add delete functionality to project show view with confirmation modal

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -24,6 +24,39 @@
     </p>
 
     <p class="text-muted">Creato il: {{ $project->created_at->format('d/m/Y') }}</p>
+    
+    <div class="d-flex gap-3 align-items-center py-4">
+        <a href="{{ route('projects.index') }}" class="btn btn-primary">Torna all'elenco</a>
 
-    <a href="{{ route('projects.index') }}" class="btn btn-primary mt-3">Torna all'elenco</a>
+        <a class="btn btn-warning" href="{{ route('projects.edit', $project->id) }}">Modifica</a>
+
+        <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Elimina
+        </button>
+
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-center">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina il progetto</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Vuoi eliminare il progetto?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                        <form action="{{ route("projects.destroy", $project->id) }}" method="POST">
+                            @csrf
+    
+                            @method("DELETE")
+                            <input type="submit" value="Elimina" class="btn btn-danger">
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
